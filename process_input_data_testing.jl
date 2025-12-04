ce2bc3f16c306ccf32cac8d1abc4218ce1f2c5a5
Vegard Kallset <vegard.viken.kallset@gmail.com>
2025-12-04 13:36:29 +0100

    Remove unused files

--- process_input_data_testing.jl
+++
@@ -1,8 +0,0 @@
-include("C:/Users/vegardvk/vscodeProjects/bernstein/process_input_data.jl")
-function test_process_input_data()
-    steps_per_hour = 4
-    scenarios = 2
-    process_load_data(steps_per_hour, scenarios)
-end
-
-test_process_input_data()
